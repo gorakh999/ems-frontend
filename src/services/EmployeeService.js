b45d69540a64a4821d8bc79a9fb554c370ca6d91
Gorakh <gorakhgupta999@gmail.com>
2024-12-08 16:31:54 +0530

    Added Add Employee feature

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -6,3 +6,7 @@ const REST_API_BASE_URL = 'http://localhost:9999/api/employees';
 export const listEmployees = () => {
     return axios.get(REST_API_BASE_URL);
 }
+
+export const createEmployee = (employee) => {
+    return axios.post(REST_API_BASE_URL, employee);
+}
